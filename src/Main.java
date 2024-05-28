af3c4c3a4e45508f9e9997da6a415e42f7538b62
Puma474Stone <shustov.arcadi@yandex.ru>
2024-05-28 12:49:05 +0700

    Redact

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
-
-        // Условным оператором проверяете, превысила ли
-        // сумма пополнения порог, и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
-
         int balance = 100;
         int amount = 1100;
         int bonusThreshold = 1000;
